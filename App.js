eda8fa8adedbccad42fc153df44a9c90d43c57e2
Reiskadog <59826923+Reiskadog@users.noreply.github.com>
2021-10-04 19:30:43 +0300

    lisätty kirjain

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>NÄKYYKÖ TÄÄ JUUUUU</Text>
+      <Text>NÄKYYKÖ TÄÄ JUUUUU 2</Text>
       <StatusBar style="auto" />
     </View>
   );
